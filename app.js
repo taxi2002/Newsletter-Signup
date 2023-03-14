fedf8b7ca9a4090f081d0f79299ca32adc17b3bc
taxiarchismintzas <taxiarchismintzas@gmail.com>
2023-03-14 12:52:13 +0200

    Second commit

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ app.post("/failure", function(req, res){
 })
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT  || 3000, function () {
     console.log("Server is runnig on port 3000")
 })
